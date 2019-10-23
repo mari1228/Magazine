2b27f8b0a998d99798a92dc2b72a8ee007d30421
mari taylor <mari.taylor1000@gmail.com>
2019-10-23 16:44:38 +1300

    updated move and position

--- site.js
+++ site.js
@@ -1,1 +1,35 @@
+$(function() {
+
+
+  function init() {
+
+
+
+    // Toggle bg colour on and off
+    $('.colour-btn').on('click', function (e) {
+      e.preventDefault();
+      $('.content').toggleClass('sm-colour--on');
+    })
+
+    // Toggle bg colour on and off
+    $('.sm-section-three-heading').on('click', function (e) {
+      e.preventDefault();
+      $('.sm-section-three-heading').toggleClass('grow');
+    })
+
+    // Toggle bg colour on and off
+    $('.move-btn').on('click', function (e) {
+      e.preventDefault();
+      $('.sm-section-one').toggleClass('sm-move');
+    })
+
+    $('.pos-btn').on('click', function (e) {
+      e.preventDefault();
+      $('.content').toggleClass('letter-spacing');
+
+    })
+  }
+
+  init();
+});
 
