352da1972ebf85f967715039c684fa52a2c9a5ae
mari taylor <mari.taylor1000@gmail.com>
2019-10-31 10:47:02 +1300

    Update site

--- site.js
+++ site.js
@@ -8,7 +8,7 @@ $(function() {
     // Toggle bg colour on and off
     $('.colour-btn').on('click', function (e) {
       e.preventDefault();
-      $('.content').toggleClass('sm-colour--on');
+      $('body').toggleClass('sm-colour--on');
     })
 
     // Toggle bg colour on and off
@@ -20,7 +20,7 @@ $(function() {
     // Toggle bg colour on and off
     $('.move-btn').on('click', function (e) {
       e.preventDefault();
-      $('.sm-section-one').toggleClass('sm-move');
+      $('.sm-section-two').toggleClass('blink');
     })
 
     $('.pos-btn').on('click', function (e) {
